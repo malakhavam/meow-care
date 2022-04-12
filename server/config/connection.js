e59816e94568e6e5f5051fd19d1042307c528b43
Maryia Malakhava <malakhava@yahoo.com>
2022-04-12 19:34:35 -0400

    fixing bugs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-comments',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/meow-care',
   {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
