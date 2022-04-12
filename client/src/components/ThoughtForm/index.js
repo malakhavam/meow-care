ef9a1276438758ecfa43f8778c91fbe46c861da0
Maryia Malakhava <malakhava@yahoo.com>
2022-04-11 21:05:18 -0400

    comments about your cat

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -69,7 +69,7 @@ const ThoughtForm = () => {
         onSubmit={handleFormSubmit}
       >
         <textarea
-          placeholder="Here's a new thought..."
+          placeholder="Few words about yourself and your cat and why are you looking for the Sitter"
           value={thoughtText}
           className="form-input col-12 col-md-9"
           onChange={handleChange}
